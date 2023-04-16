b4b0c86b1aab3e458ab060632b8153a6f42afba4
Ruben <ruben@volware.nl>
2023-04-17 00:19:05 +0200

    assets and data processing

--- gulpfile.js
+++ gulpfile.js
@@ -4,10 +4,38 @@ const sass = require('gulp-sass')(require('sass'));
 const inlineCss = require('gulp-inline-css');
 const connect = require('gulp-connect');
 const cleanCSS = require('gulp-clean-css');
+const replace = require('gulp-replace');
 const browserSync = require('browser-sync').create();
 const fs = require('fs');
 const path = require('path');
 const outputDir = 'dist';
+
+function loadJsonData(jsonPath) {
+    if (fs.existsSync(jsonPath)) {
+    const rawData = fs.readFileSync(jsonPath, 'utf8');
+    return JSON.parse(rawData);
+    }
+    return {};
+}
+
+function flattenObject(obj, prefix = '') {
+    return Object.keys(obj).reduce((acc, k) => {
+    const pre = prefix.length ? prefix + '.' : '';
+    if (typeof obj[k] === 'object') {
+        Object.assign(acc, flattenObject(obj[k], pre + k));
+    } else {
+        acc[pre + k] = obj[k];
+    }
+    return acc;
+    }, {});
+}
+
+
+function copyAssets() {
+    return src('src/assets/**/*')
+      .pipe(dest(`${outputDir}/assets`))
+  }
+  
 
 function compileSass() {
   return src('src/styles/**/*.scss', { base: 'src/styles' })
@@ -17,43 +45,63 @@ function compileSass() {
 }
 
 function compilePug(done) {
-  const pugFiles = fs.readdirSync('src/templates').filter(file => path.extname(file) === '.pug');
+        const pugFiles = fs.readdirSync('src/templates').filter(file => path.extname(file) === '.pug');
+        const useJsonData = true; // Set this to false to keep variables in the HTML
+    
+        Promise.all(
+        pugFiles.map(file => {
+            const fileName = path.basename(file, '.pug');
+            const generalCssContent = fs.readFileSync(`${outputDir}/styles/styles.css`, 'utf8');
+            const templateCssPath = `${outputDir}/styles/${fileName}.css`;
+            let templateCssContent = '';
+    
+            if (fs.existsSync(templateCssPath)) {
+            templateCssContent = fs.readFileSync(templateCssPath, 'utf8');
+            }
+    
+            const combinedCssContent = generalCssContent + templateCssContent;
 
-  Promise.all(
-    pugFiles.map(file => {
-      const fileName = path.basename(file, '.pug');
-      const generalCssContent = fs.readFileSync(`${outputDir}/styles/styles.css`, 'utf8');
-      const templateCssPath = `${outputDir}/styles/${fileName}.css`;
-      let templateCssContent = '';
+            // Load JSON data
+            const jsonData = useJsonData ? flattenObject(loadJsonData(`src/data/${fileName}.json`)) : {};
 
-      if (fs.existsSync(templateCssPath)) {
-        templateCssContent = fs.readFileSync(templateCssPath, 'utf8');
-      }
+        let pugStream = src(`src/templates/${fileName}.pug`)
+            .pipe(pug({ locals: { cssContent: combinedCssContent }, pretty: true }))
+            .pipe(inlineCss({
+            applyStyleTags: true,
+            applyLinkTags: true,
+            removeStyleTags: false,
+            removeLinkTags: true,
+            preserveMediaQueries: true,
+            }));
 
-      const combinedCssContent = generalCssContent + templateCssContent;
+        // Replace variables with JSON data if useJsonData is true
+        if (useJsonData) {
+            Object.entries(jsonData).forEach(([key, value]) => {
+            const regex = new RegExp(`{{\\s*${key}\\s*}}`, 'g');
+            pugStream = pugStream.pipe(replace(regex, value));
+            });
+        }
 
-      return src(`src/templates/${fileName}.pug`)
-        .pipe(pug({ locals: { cssContent: combinedCssContent }, pretty: true }))
-        .pipe(inlineCss({
-          applyStyleTags: true,
-          applyLinkTags: true,
-          removeStyleTags: false,
-          removeLinkTags: true,
-          preserveMediaQueries: true,
-        }))
-        .pipe(dest(`${outputDir}`))
-        .pipe(browserSync.stream());
-    })
-  ).then(() => done());
+        return pugStream
+            .pipe(dest(`${outputDir}`))
+            .pipe(browserSync.stream());
+        })
+    ).then(() => done());
 }
 
-function compileIndexPug() {
-  const templates = fs.readdirSync('src/templates').filter(file => path.extname(file) === '.pug');
-  return src('src/index.pug')
-    .pipe(pug({ locals: { templates }, pretty: true }))
-    .pipe(dest(`${outputDir}`))
-    .pipe(browserSync.stream());
-}
+  function compileIndexPug() {
+    const templates = fs.readdirSync('src/templates').filter(file => path.extname(file) === '.pug');
+    const useJsonData = true; // Set this to false to keep variables in the HTML
+  
+    // Load JSON data
+    const jsonData = useJsonData ? loadJsonData('src/data/index.json') : {};
+  
+    return src('src/index.pug')
+      .pipe(pug({ locals: { templates, ...jsonData }, pretty: true }))
+      .pipe(dest(`${outputDir}`))
+      .pipe(browserSync.stream());
+  }
+  
 
 function serve(done) {
   connect.server({
@@ -65,11 +113,14 @@ function serve(done) {
     proxy: 'localhost:8080',
   });
 
+  watch('src/data/**/*.json', series(compileSass, compilePug, compileIndexPug)).on('change', browserSync.reload);
   watch('src/templates/**/*.pug', series(compileSass, compilePug, compileIndexPug)).on('change', browserSync.reload);
   watch('src/index.pug', series(compileIndexPug)).on('change', browserSync.reload);
   watch('src/styles/**/*.scss', series(compileSass, compilePug, compileIndexPug)).on('change', browserSync.reload);
+  watch('src/assets/**/*', series(copyAssets)).on('change', browserSync.reload);
+  
 
   done();
 }
 
-exports.default = series(compileSass, compilePug, compileIndexPug, serve);
+exports.default = series(compileSass, compilePug, compileIndexPug, copyAssets, serve);
